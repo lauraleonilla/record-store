2a1ceda793724198b3b6c95fe97c8aff1d798e67
ext-laura.forsblom@innofactor.com <lauralforsblom@gmail.com>
2023-11-08 11:25:28 +0200

    Added db and made search field query on change

--- frontend/src/SearchField.jsx
+++ frontend/src/SearchField.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const SearchField = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const apiUrl = 'http://localhost:3001/search';
+    const fetchData = async () => {
+      const response = await axios.post(apiUrl, { searchTerm });
+      console.log('Data', response);
+    };
+    if (searchTerm && searchTerm.length > 2) {
+      fetchData();
+    }
+  }, [searchTerm]);
+
   return (
-    <StyledSearchField>
-      <p>Hae artistia tai albumia...</p>
-    </StyledSearchField>
+    <StyledSearchField
+      onChange={(e) => setSearchTerm(e.target.value)}
+      placeholder="Hae artistia tai albumia..."
+    />
   );
 };
 
-const StyledSearchField = styled.div`
+const StyledSearchField = styled.input`
   background-color: ${(props) => props.theme.white};
   height: 32px;
   width: 500px;
-  position: relative;
   border-radius: 5px;
-  box-shadow: -1px 1px 6px 0px rgba(0, 0, 0, 0.75) inset;
+  box-shadow: -2px 0px 6px -1px rgba(0, 0, 0, 0.75) inset;
+  padding-left: 10px;
 `;
 
 export default SearchField;
