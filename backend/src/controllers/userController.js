8aa679e3188bf5b95007f4fae1cdd05e81f40b94
Laura Leonilla Forsblom <46851815+lauraleonilla@users.noreply.github.com>
2023-11-28 14:17:46 +0200

    Validation etc fixed (#35)
    * Fixed validation
    
    * Fixed displaying errors
    
    * Added api url as en variable
    
    * Fixed api url
    
    * Fixed url
    
    * added vercel.json
    
    * testing

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,5 +1,5 @@
-import { pool } from '../../index.js';
-import bcrypt from 'bcrypt';
+import { pool } from "../../index.js";
+import bcrypt from "bcryptjs";
 
 export async function validateEmail(req, res) {
   let client = null;
@@ -7,38 +7,59 @@ export async function validateEmail(req, res) {
   try {
     client = await pool.connect();
   } catch (err) {
-    console.log('Error connecting to database: ', err);
+    console.log("Error connecting to database: ", err);
     return;
   }
   try {
-    const emailData = await client.query('SELECT email FROM users WHERE email = $1', [email]);
+    const emailData = await client.query(
+      "SELECT email FROM users WHERE email = $1",
+      [email]
+    );
     res.send(JSON.stringify(emailData.rows)).status(200);
   } catch (err) {
-    console.error('Error while validating email:', err);
+    console.error("Error while validating email:", err);
   } finally {
     client.release();
   }
 }
 
 export async function registerUser(req, res) {
-  const { email, firstName, lastName, address, postalCode, city, phoneNumber, password } = req.body;
+  const {
+    email,
+    firstName,
+    lastName,
+    address,
+    postalCode,
+    city,
+    phoneNumber,
+    password,
+  } = req.body;
   let client = null;
   try {
     client = await pool.connect();
   } catch (err) {
-    console.log('Error connecting to database: ', err);
+    console.log("Error connecting to database: ", err);
     return;
   }
   const hashedPw = await bcrypt.hash(password, 10);
   const query = {
-    text: 'INSERT INTO users (email, firstName, lastName, street, postalCode, city, phoneNum, password) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *',
-    values: [email, firstName, lastName, address, postalCode, city, phoneNumber, hashedPw],
+    text: "INSERT INTO users (email, firstName, lastName, street, postalCode, city, phoneNum, password) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
+    values: [
+      email,
+      firstName,
+      lastName,
+      address,
+      postalCode,
+      city,
+      phoneNumber,
+      hashedPw,
+    ],
   };
   try {
     const createdUser = await client.query(query);
     res.send(JSON.stringify(createdUser.rows)).status(200);
   } catch (err) {
-    console.error('Error when registering new user to db: ', err);
+    console.error("Error when registering new user to db: ", err);
   } finally {
     client.release();
   }
